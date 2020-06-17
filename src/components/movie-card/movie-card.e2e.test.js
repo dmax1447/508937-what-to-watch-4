f78741aa7329480cd88b4b70dba7fbc1c8c69237
Max Dragaytsev <dmax1447@gmail.com>
2020-06-17 22:32:26 +0300

    добавит e2e тест для MovieCard

---
+++ src/components/movie-card/movie-card.e2e.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import MovieCard from './movie-card.jsx';
+
+import films from '../../mocks/tests/films.js';
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+it(`when mouse hover over movie card callback should be called with movie name`, () => {
+  const cardTitleClickHandler = jest.fn();
+  const cardHoverHandler = jest.fn();
+
+  const movie = films[0];
+  const movieCard = shallow(
+      <MovieCard movie={movie} onCardTitleClick={cardTitleClickHandler} onMouseEnter={cardHoverHandler} />
+  );
+
+  movieCard.simulate(`mouseEnter`);
+  expect(cardHoverHandler.mock.calls[0][0]).toBe(movie.title);
+});
