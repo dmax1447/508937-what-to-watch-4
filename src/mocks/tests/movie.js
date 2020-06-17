39489c96aeb605f6329adcdd290d4eb8e7384b02
Max Dragaytsev <dmax1447@gmail.com>
2020-06-17 17:49:20 +0300

    delete all for update case

--- src/mocks/tests/movie.js
+++
@@ -1,7 +0,0 @@
-const movieMock = {
-  id: `f01`,
-  title: `Fantastic Beasts: The Crimes of Grindelwald`,
-  picture: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
-};
-
-export default movieMock;
--- src/mocks/tests/promo.js
+++
@@ -1,7 +0,0 @@
-const promo = {
-  title: `Birds`,
-  genre: `horror`,
-  year: `1963`,
-};
-
-export default promo;
